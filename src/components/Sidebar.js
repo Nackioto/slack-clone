23aad21798d2814053a9285a87b02f730ab79b2d
Ignacio <ignacio.puglisi@gmail.com>
2021-02-24 00:11:12 +0000

    channel section

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import { sidebarItemsData } from '../data/SidebarData';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
@@ -14,10 +16,32 @@ function Sidebar() {
                 </NewMessage>
             </WorkspaceContainer>
             <MainChannels>
-                <MainChannelItem>
-
-                </MainChannelItem>
+                {
+                    sidebarItemsData.map(item => (
+                        <MainChannelItem>
+                            {item.icon}
+                            {item.text}
+                        </MainChannelItem>
+                    ))
+                }
             </MainChannels>
+            <ChannelsContainer>
+                <NewChannelContainer>
+                    <div>
+                        Channels
+                    </div>
+                    <AddIcon />
+
+                </NewChannelContainer>
+                <ChannelsList>
+                    <Channel>
+                        # Channel 1
+                  </Channel>
+                    <Channel>
+                        # Channel 2
+                  </Channel>
+                </ChannelsList>
+            </ChannelsContainer>
         </Container>
     )
 }
@@ -53,7 +77,7 @@ const NewMessage = styled.div`
   cursor: pointer;
 `
 const MainChannels = styled.div`
-  
+  padding-top: 20px;
 `
 const MainChannelItem = styled.div`
   color: rgb(188, 171, 188);
@@ -63,4 +87,33 @@ const MainChannelItem = styled.div`
   align-items: center;
   padding-left: 19px;
   cursor: pointer;
+  :hover {
+      background: #350D36;
+  }
+
+`
+const ChannelsContainer = styled.div`
+  color: rgb(188,171,188);
+  margin-top: 10px;
+  
+`
+const NewChannelContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 28px;
+  padding-left: 19px;
+  padding-right: 12px;
+`
+const ChannelsList = styled.div`
+`
+const Channel = styled.div`
+  height: 28px;
+  display: flex;
+  align-items: center;
+  padding-left: 19px;
+  cursor: pointer;
+  :hover {
+      background: #350D36;
+  }
 `
